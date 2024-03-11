a49f80ad3a69e0d767778c1dbb73ef1037a6b824
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-11 17:36:15 -0400

    Created lesson content

---
+++ Lesson.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------
+# Name:		    Lesson 3.2 Variables & Data Types
+# Purpose:		Lesson Examples
+# Author:		Chen. C
+#
+# Created:		11/03/2024
+#-------------------------------------------------------------------------
+
+'''
+Using Variables
+'''
+# Example #1: Greeting
+
+
+# Example #2: My Number
+
+
+'''
+Finding Out the Data Type
+'''
+# Example #1: 50.0 Type
+
+
+# Example #2: 50 Type
