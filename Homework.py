87af2ec0325c738a1c728572e256b8638561109e
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-11 17:36:03 -0400

    Created homework Qs

---
+++ Homework.py
@@ -0,0 +1,32 @@
+#-------------------------------------------------------------------------
+# Name:		    Lesson 3.2 Variables & Data Types
+# Purpose:		Homework Questions
+# Author:		Last Name. First Initial
+#
+# Created:		dd/mm/yyyy
+#-------------------------------------------------------------------------
+
+# QUESTION #1:
+# Identify the data type for each of the following
+
+num1 = 32.0 # delete this and put your answer here
+num2 = 32 # delete this and put your answer here
+num3 = "32" # delete this and put your answer here
+
+# QUESTION #2:
+# Identify the follwing three parts for the variable below
+mark = 82.5
+# Name: 
+# Type: 
+# Value: 
+
+# QUESTION #3:
+# Create your own variable (make sure to follow the rules and best practices!)
+# and assign it to a value with a data type of your choice
+
+
+# QUESTION #4:
+# How can you check the data type of a variable if you are unsure?
+'''
+Delete this and write your answer in this multi-line comment
+'''
